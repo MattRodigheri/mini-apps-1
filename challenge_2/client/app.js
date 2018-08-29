58ce277321946148302a32634b04958e486d8ac6
MattRodigheri <mrodigheri@gmail.com>
2018-08-28 17:36:33 -0700

    get request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,32 @@
 
+ $( document ).ready(function() {
 
+    $( "form" ).click(function( event ) {
+      event.preventDefault();
+    });
+
+  $( ".submit" ).click(function(event) {
+    var data = $( "textarea" ).val();
+    $.ajax({
+      type : "POST",
+      url: 'http://localhost:3000/samples',
+      data : JSON.stringify(data),//fix this #
+      success : function(result) {
+        console.log("Success: ", result);
+        $.ajax({
+          type : "GET",
+          url: 'http://localhost:3000/samples',
+          success: function(result){
+            console.log("Success: ", result);
+          },
+          error: function(error) {
+            console.log("ERROR: ", error);
+          }
+        });
+      },
+      error : function(error) {
+        console.log("ERROR: ", error);
+      }
+    });
+  });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,34 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
+app.use(express.static('./client'));
 
+//GET method route
+app.get('/samples', function (req, res) {
+  fs.readFile('./samples/csv_report.csv', (err, data) => {
+    if (err) {
+      res.status(500)
+      res.send(err)
+    } else {
+      console.log('successful get')
+      res.send(data)
+    }
+  })
+})
+
+// POST method route
+app.post('/samples', function (req, res) {
+  //take in data from form
+  //var data = 'hi';
+  //if (err) {
+  //   res.status(500)
+  //   res.send(err)
+  // } else {
+    //console.log('hey')
+    res.status(201).send(req.data)
+  //}
+})
+
+app.listen(3000, () => console.log('listening on port 3000!'))
