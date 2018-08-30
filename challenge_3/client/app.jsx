302a3a2b08e2c0dc57d80e4c6899bb55a26cb58b
MattRodigheri <mrodigheri@gmail.com>
2018-08-29 20:16:57 -0700

    forms rendering correctly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
       expirydate: '',
       cvv: 0,
       billingzip: 0,
-      checkoutButton: false,
+      checkoutButton: true,
       form1NextButton: false,
       form2NextButton: false,
       form3NextButton: false,
@@ -30,38 +30,74 @@ class App extends React.Component {
 
   //function to go from home -> form1
   checkout() {
-    console.log('checkout button clicked')
+    this.setState({
+      checkoutButton: false,
+      form1NextButton: true
+    }, () => {console.log("checkout ", this.state.checkoutButton);})
   }
   //function to go from form1 -> form2
   form1Next() {
-
+    this.setState({
+      form1NextButton: false,
+      form2NextButton: true
+    }, () => {console.log("form1 ",this.state.form1NextButton);})
   }
   //function to go from form2 -> form3
   form2Next() {
-
+    this.setState({
+      form2NextButton: false,
+      form3NextButton: true
+    }, () => {console.log("form2 ", this.state.form2NextButton);})
   }
   //function to go from form3 -> purchase
   form3Next() {
-
+    this.setState({
+      form3NextButton: false,
+      purchaseButton: true
+    }, () => {console.log("form3 ", this.state.form3NextButton);})
   }
   //function to go from purchase -> home
   purchase() {
-
+    this.setState({
+      purchaseButton: false,
+      checkoutButton: true
+    }, () => {console.log("purchase ", this.state.purchaseButton);})
   }
 
   render() {
-    return (
-      <div>
-        <h1>Checkout App</h1>
-        <Home />
-        <Form1 />
-        <Form2 />
-        <Form3 />
-        <Confirmation />
-      </div>
-    )
+    if (this.state.checkoutButton) {
+      return (
+        <div>
+          <h1>Checkout App</h1>
+          <Home checkout={this.checkout}/>
+        </div>
+      )
+    }
+    else if (this.state.form1NextButton){
+      return (
+        <Form1 form1={this.form1Next}/>
+      )
+    }
+    else if (this.state.form2NextButton) {
+      return (
+        <Form2 form2={this.form2Next}/>
+      )
+    }
+    else if (this.state.form3NextButton) {
+      return (
+        <Form3 form3={this.form3Next}/>
+      )
+    }
+    else if (this.state.purchaseButton) {
+      return (
+        <Confirmation purchase={this.purchase}/>
+      )
+    }
+    // else {
+    //   this.state.checkoutButton = true;
+    // }
   }
-}
+};
 
 function Home(props) {
   return (
@@ -78,7 +114,7 @@ function Form1(props) {
       <div>Name:<input type="text" /></div>
       <div>Email:<input type="text" /></div>
       <div>Password:<input type="text" /></div>
-      <button id="form1NextButton" onClick={props.form1Next}>Next</button>
+      <button id="form1NextButton" onClick={props.form1}>Next</button>
     </div>
   )
 }
@@ -92,7 +128,7 @@ function Form2(props) {
       <div>State:<input type="text" /></div>
       <div>Zip Code:<input type="text" /></div>
       <div>Phone Number:<input type="text" /></div>
-      <button id="form2NextButton" onClick={props.form2Next}>Next</button>
+      <button id="form2NextButton" onClick={props.form2}>Next</button>
     </div>
   )
 }
@@ -104,7 +140,7 @@ function Form3(props) {
       <div>Expiry Date:<input type="text" /></div>
       <div>CVV:<input type="number" /></div>
       <div>Billing Zip Code:<input type="number" /></div>
-      <button id="form3NextButton" onClick={props.form3Next}>Next</button>
+      <button id="form3NextButton" onClick={props.form3}>Next</button>
     </div>
   )
 }
